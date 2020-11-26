e8a2085b3aa5e04dc8678b7a7c8800f9a5bf6595
Eon Jo <34697086+sim0417@users.noreply.github.com>
2020-11-26 21:40:39 +0900

    [Update] Create & Read data from firebase

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,42 @@
-import React, { useState } from "react";
+import { dbService } from "fbManager";
+import React, { useState, useEffect } from "react";
 
-const Home = () => {
+const Home = ({ userObj }) => {
   const [tweety, setTweety] = useState();
+  const [tweetys, setTweetys] = useState([]);
 
-  const onSubmit = (event) => {
+  // const loadTweetys = async () => {
+  //   const collection = await dbService.collection("tweety").get();
+  //   collection.forEach((doc) => {
+  //     const tweetyObject = {
+  //       ...doc.data(),
+  //       id: doc.id,
+  //     };
+  //     setTweetys((prev) => [tweetyObject, ...prev]);
+  //   });
+  // };
+
+  useEffect(() => {
+    // loadTweetys();
+
+    dbService.collection("tweety").onSnapshot((snapshot) => {
+      const tweetyArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setTweetys(tweetyArray);
+    });
+  }, []);
+
+  const onSubmit = async (event) => {
     event.preventDefault();
+
+    await dbService.collection("tweety").add({
+      text: tweety,
+      createdAt: Date.now(),
+      createId: userObj.uid,
+    });
+    setTweety("");
   };
 
   const onChange = (event) => {
@@ -14,12 +46,29 @@ const Home = () => {
     setTweety(value);
   };
 
+  // console.log(tweetys);
+  // console.log(userObj);
+
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input type="text" placeholder="" value={tweety} onChange={onChange} maxLength={120} required />
+        <input
+          type="text"
+          placeholder="What's on your mind ?"
+          value={tweety}
+          onChange={onChange}
+          maxLength={120}
+          required
+        />
         <input type="submit" value="Tweety!" />
       </form>
+      <div>
+        {tweetys.map((data) => (
+          <div key={data.id}>
+            <h4>{data.text}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
